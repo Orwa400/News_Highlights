40f2d007e0fe490b6fda6f0f9872c76a5cc8d90c
Orwa400 <matheworwa3@gmail.com>
2024-01-16 19:40:08 +0300

    Add NewsArticle database

--- models.py
+++ models.py
@@ -8,4 +8,13 @@ class NewsSource(db.Model):
     name = db.Column(db.String(50), nullable=False)
     category = db.Column(db.String(50), nullable=False)
     articles = db.relationship('NewsArticle', backref='source', lazy=True)
+
+class NewsArticle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    image =db.Column(db.String(100))
+    created_at = db.Column(db.DataTime, default=datetime.utcnow)
+    source_id = db.Column(db.Integer, db.ForeignKey('news_source.id'), nullbale=False)
+
     
