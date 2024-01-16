1368feb8538718ac8522828510aa2cfd8d7910b6
Orwa400 <matheworwa3@gmail.com>
2024-01-16 19:15:24 +0300

    Add dictionary and favorites

--- app.py
+++ app.py
@@ -4,12 +4,23 @@ from models import db, NewsSource, NewsArticle
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SECRET_KEY'] = '1738' 
 db.init_app(app)
 
 @app.rote('/')
 def home():
-    sources = NewsSource.query.all()
-    return render_template ('index.html', sources=sources)
+   sources_by_category = {} # DIctionary to organize sources by category
+   categories = set()
+   
+   sources = NewsSource.query.all()
+   for source in sources:
+        categories.add(source.category)
+        if source.category not in sources_by_category:
+            sources_by_category[source.category] = [source]
+        else:
+            sources_by_category[source.category].append(source)
+
+return render_template('index.html', sources_by_category=sources_by_category, categories=categories)
 
 @app.route('/news/<source_id>')
 def news_source(source_id):
@@ -22,6 +33,19 @@ def news_article(article_id):
     article = NewsArticle.query.get(article_id)
     return render_template('news_article.html', article=article)
 
+@app.route('/add_favorite/<source_id>')
+def add_favorite(source_id):
+    if 'favorites' not in session:
+        session['favorites'] = []
+
+    if souce_id not in session['favorites']:
+        session['favorites'].append(source_id)
+        flash('News source added to favorites!', 'success')
+    else:
+        flash('News source is already in favorites!', 'info')
+
+    return redirect(url_for('home'))
+    
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
